a549e9c38f1914531481f3501d24d71d980eb9a5
Your Name <you@example.com>
2025-09-30 07:10:49 -0300

    feature : adicao do calendario e compras do dia

---
+++ Components/Classes/DiaCalendario.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PouPixFront.Components
+{
+    public class DiaCalendario
+    {
+        public DateTime Data { get; set; }
+        public decimal ValorPermitido { get; set; }
+    }
+}
--- Response/HistoricoCompraResponse.cs
+++ Response/HistoricoCompraResponse.cs
@@ -8,10 +8,10 @@ namespace SiteBlazor.Response
     public class HistoricoCompraResponse
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
+        public string Nome { get; set; } = string.Empty;
         public decimal Valor { get; set; }
         public int IdCategoria { get; set; }
-        public string Categoria { get; set; }
+        public string Categoria { get; set; } = string.Empty;
         public DateTime Data { get; set; }
         public string? Comentario { get; set; }
     }
